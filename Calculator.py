03703a2cba6b3cf64a97e7736daedb29bf577484
Dilan Samarasinghe <covertskippy55@gmail.com>
2014-07-10 08:50:34 -0400

    Used grid instead of pack in order to get a vertical alignment for the buttons

--- Calculator.py
+++ Calculator.py
@@ -7,19 +7,23 @@ class Calculator:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        self.addButton = Button(frame, text = '+', command = self.add)
-        self.addButton.pack(side = RIGHT)
-        self.subtractButton = Button(frame, text = "-", command = frame.quit)
-        self.subtractButton.pack(side = RIGHT)
-        self.multiplyButton = Button(frame, text = '*', command = frame.quit)
-        self.multiplyButton.pack(side = RIGHT)
-        self.divideButton = Button(frame, text = "/", command = frame.quit)
-        self.divideButton.pack(side = RIGHT)
+        Button(frame, text = "/", command = self.divide).grid(row = 0, column = 0)
+        Button(frame, text = '*', command = self.multiply).grid(row = 1, column = 0)
+        Button(frame, text = "-", command = self.subtract).grid(row = 2, column = 0)
+        Button(frame, text = '+', command = self.add).grid(row = 3, column = 0)
+
 
     def add(self):
-        x = int(raw_input("Please enter the first number to be added: "))
-        y = int(raw_input("Please enter the second number to be added: "))
-        return x + y
+        pass
+
+    def subtract(self):
+        pass
+
+    def multiply(self):
+        pass
+
+    def divide(self):
+        pass
 
 
 root = Tk()
@@ -27,5 +31,5 @@ Calc = Calculator(root)
 root.mainloop()
 try:
     root.destroy()
-except Exception, e:
-    print 'Error: %s' % (e)
+except Exception:
+    pass
